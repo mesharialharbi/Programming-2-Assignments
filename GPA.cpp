6ab323f693daed5a0090cdd5cebc76dc8ee56c92
mesharialharbi <meshari.ah@gmail.com>
2015-09-06 01:49:44 +0300

    Update GPA.cpp

--- GPA.cpp
+++ GPA.cpp
@@ -1,18 +1,41 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-
-// A* > 95
-// A > 90
-// B* > 85
-// B > 80
-// C* > 75
-// C > 70
-// D* > 65
-// D > 60
-
-    return 0;
+#include <iostream> 
+using namespace std; int main() 
+{ 
+    int grade; 
+    cout << "Enter your grade: "; 
+    cin >> grade; 
+  
+    if (grade > 100 || grade < 0) 
+        cout << "Error ...\n "; 
+    else
+    if (grade >= 95 && grade <= 100) 
+        cout << "You got Grade A+\n"; 
+    else
+    if (grade< 95 && grade>89) 
+        cout << "You got Grade A\n"; 
+    else
+    if (grade<90  grade>84) 
+        cout << "You got Grade B+\n"; 
+    else
+    if (grade<85  grade>79) 
+        cout << "You got Grade B\n"; 
+    else
+    if (grade<80  grade>74) 
+        cout << "You got Grade C+\n"; 
+    else
+    if (grade<75 grade>69) 
+        cout << "You got Grade C\n"; 
+    else
+    if (grade<70  grade>64) 
+        cout << "You got Grade D+\n"; 
+    else
+    if (grade<65  grade>59) 
+        cout << "You got Grade D\n"; 
+    else
+    if (grade<60  grade >= 0) 
+        cout << "You got Grade F\n"; 
+      
+  
+    system("pause"); 
+    return 0; 
 }
